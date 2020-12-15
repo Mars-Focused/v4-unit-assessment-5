f3557e4d125e6aec3b24d1991ca586a677f8d5e4
Mars_Clarke <marsthecoder@outlook.com>
2020-12-15 10:39:25 -0700

    unsure how to debug this

--- server/index.js
+++ server/index.js
@@ -1,23 +1,38 @@
-require('dotenv').config();
-const express = require('express'),
-      userCtrl = require('./controllers/user'),
-      postCtrl = require('./controllers/posts')
+require("dotenv").config();
+const express = require("express"),
+  userCtrl = require("./controllers/user"),
+  postCtrl = require("./controllers/posts");
+const massive = require("massive");
 
+const { CONNECTION_STRING, SERVER_PORT } = process.env;
 
 const app = express();
 
 app.use(express.json());
+// app.use();
 
 //Auth Endpoints
-app.post('/api/auth/register', userCtrl.register);
-app.post('/api/auth/login', userCtrl.login);
-app.get('/api/auth/me', userCtrl.getUser);
-app.post('/api/auth/logout', userCtrl.logout);
+app.post("/api/auth/register", userCtrl.register);
+app.post("/api/auth/login", userCtrl.login);
+app.get("/api/auth/me", userCtrl.getUser);
+app.post("/api/auth/logout", userCtrl.logout);
 
 //Post Endpoints
-app.get('/api/posts', postCtrl.readPosts);
-app.post('/api/post', postCtrl.createPost);
-app.get('/api/post/:id', postCtrl.readPost);
-app.delete('/api/post/:id', postCtrl.deletePost)
+app.get("/api/posts", postCtrl.readPosts);
+app.post("/api/post", postCtrl.createPost);
+app.get("/api/post/:id", postCtrl.readPost);
+app.delete("/api/post/:id", postCtrl.deletePost);
 
-app.listen(4000, _ => console.log(`running on ${4000}`));
+massive({
+  connectionString: CONNECTION_STRING,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+}).then((dbInstance) => {
+  app.set("db", dbInstance);
+  console.log("DB Ready Nya~");
+
+  app.listen(SERVER_PORT, () =>
+    console.log(`Running on ${SERVER_PORT} Master Nya~`)
+  );
+});
