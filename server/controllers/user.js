73e128893d6183b5b0486d94d66be7adb0b9c8d3
Mars_Clarke <marsthecoder@outlook.com>
2020-12-15 12:23:58 -0700

    Lunch

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,3 +1,5 @@
+const bcrypt = require("bcryptjs");
+
 module.exports = {
   register: async (req, res) => {
     const db = req.app.get("db");
@@ -8,5 +10,15 @@ module.exports = {
     if (existingUser) {
       return res.status(409).send("User already exists");
     }
+
+    const salt = bcrypt.genSaltSync(10);
+
+    const hash = bcrypt.hashSync(password, salt);
+
+    const [newUser] = await db.register_user([id, username, hash, admin]);
+
+    req.session.user = newUser;
+
+    res.status(200).send(newUser);
   },
 };
